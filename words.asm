6f715e19eac02606d8d623f704c9093edda85892
Richard <rr.aggethic@gmail.com>
2026-06-13 19:36:00 -0500

    Addition 1

---
+++ words.asm
@@ -0,0 +1,27 @@
+/*  
+We as humans have arrived at a salient moment. An intangible sensation hangs in the air, invisible, but highly anxiogenic.
+The stench of rot surrounds and overwhelms us. We search for the source in order to remove it, but we find it pervasive.
+What is the cause of this disillusion? Have we stagnated as a species?
+*/
+
+/*  
+Each and every one of us has been poisoned by our own minds. A great amount of toxicity has built up within the collective consciousness of humanity.
+The toxin within all of us is belief. Not religious belief, the whole of it. The concept of certainty is fallacious.
+This is no novel proposition, but the human collective has not properly recognized it. Reality, or Actuality, most probably, is not deterministic.
+Rather, humanity has progressively unearthed information which leads to the hypothesis that our existence is probablistic.
+*/
+
+/*  
+Animism, Theology, Cosmology, Elenchus, Aristotelianism, Platonism, Rationalism, Existentialism, Postmodernism, all of metaphysics has existed as a human effort to give definition to our existence.
+If reality is indeed flexible, our definitions ought to reconcile this. Each of our mental definitions exist as an aggregate of correlations to other definitions.
+These correlations, if probablistic, should contain weights. Grass = [(green: 0.2), (plant: 0.9), ...].
+These weights, or probabilites, express the accuracy of a given correlation to the definition which it constructs.
+Grass is almost always a plant, thus this correlation would have a high weight, but it is much less frequently green. Plato's "Forms", or weights of 1, do not exist in a probablistic model of reality. 
+Rather, a probability without a false case would be expressed aysmptotically as to not preclude undiscovered information.
+*/
+
+/*
+The preceding suggestion is enormous. The construction of such a map of information would be infinite, and thus impossible.
+This script is not a prescription for the creation of such a map, though its undertaking is not discouraged.
+Rather, the focus of this script is upon the application of the preceding suggestion upon the human conscious individually and collectively.
+*/
